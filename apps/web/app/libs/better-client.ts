3f7a6c5567312da48a6306fad26df8998cad17ba
soda <soda@sodadeMacBook-Air.local>
2025-01-11 19:03:40 +0800

    打印appurl

--- apps/web/app/libs/better-client.ts
+++ apps/web/app/libs/better-client.ts
@@ -1,5 +1,6 @@
-import { createAuthClient, z } from "better-auth/react"
+import { createAuthClient } from "better-auth/react"
 import { adminClient, inferAdditionalFields, usernameClient } from "better-auth/client/plugins"
+console.log('@@@@process.env.APP_URL',process.env.APP_URL);
 export const authClient =  createAuthClient({
     baseURL: process.env.APP_URL ?? 'http://localhost:3000',
     plugins: [
