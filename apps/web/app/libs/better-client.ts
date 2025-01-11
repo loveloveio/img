f962c3a3b56ee8a52a707dcc58430a1896184446
soda <soda@sodadeMacBook-Air.local>
2025-01-11 19:08:45 +0800

    x

--- apps/web/app/libs/better-client.ts
+++ apps/web/app/libs/better-client.ts
@@ -1,7 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 import { adminClient, inferAdditionalFields, usernameClient } from "better-auth/client/plugins"
 export const authClient =  createAuthClient({
-    baseURL: 'http://localhost:3001',
     plugins: [
         adminClient(),
         usernameClient(),
