af63e10dccda27135ec7adc2fed0c49379d37647
soda <soda@sodadeMacBook-Air.local>
2025-01-15 06:18:37 +0800

    启动

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
+    // {
+    //   name: 'meilisearch',
+    //   script: 'meilisearch',
+    //   args: '--http-addr 127.0.0.1:7700',
+    //   env: {
+    //     MEILI_ENV: 'development',
+    //     MEILI_MASTER_KEY: 'jtxLEA5ea-WyPaWJYb6MXVlZJx2mqocsbQ5idz7-X20'
+    //   }
+    // },
     {
-      name: 'meilisearch',
-      script: 'meilisearch',
-      args: '--http-addr 127.0.0.1:7700',
-      env: {
-        MEILI_ENV: 'development',
-        MEILI_MASTER_KEY: 'jtxLEA5ea-WyPaWJYb6MXVlZJx2mqocsbQ5idz7-X20'
-      }
-    },
-    {
-      name: 'web',
+      name: 'bs-web',
       cwd: './apps/web',
       script: 'yarn',
-      args: 'start --port 3000',
+      args: 'start --port 3001',
       env: {
         NODE_ENV: 'development'
       }
